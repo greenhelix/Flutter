e5bc8f9631aabdd5c52147d34d7f52e7d10a356c
greenhelix <dlrghks4444@gmail.com>
2020-04-09 16:14:44 +0900

    경로 상세 화면 수정 먼저 할 것
    - 이미지 업로드 방법 flutter image picker 사용 할 것

--- LoginPage/flutterPrac/tracker/lib/data/data_detail.dart
+++ LoginPage/flutterPrac/tracker/lib/data/data_detail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tracker/custom/login_back.dart';
 
 class DataDetailLayout extends StatefulWidget {
   @override
@@ -8,13 +9,31 @@ class DataDetailLayout extends StatefulWidget {
 class _DataDetailLayoutState extends State<DataDetailLayout> {
   @override
   Widget build(BuildContext context) {
+    final Size background = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('세부설정 화면'),
       ),
-      body: Container(
-        color: Colors.amberAccent,
-        child: Center(child: Text('세부설정 화면입니다.')),
+      body: Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  //저장시 입력되는 값들, 필드값이 들어오게 하는 부분
+                  // 사진 등록 가능하게
+                  // 경로이름
+                  // 날짜
+                  // 거리
+                  // 수단
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
