5118452b3c4c05217317e2f3ce0595174ecffe17
Ik Hwan <dlrghks4444@gmail.com>
2020-06-12 18:20:20 +0900

    covide board start 06/12
    covide board start 06/12

---
+++ FlutterProject/flutterPrac/coronaSituation/covid_board/lib/board/now_korea.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+//현재 우리나라의 상황을 보여줍니다.
+class NowBoardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
