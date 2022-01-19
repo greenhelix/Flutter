a08204c59b10854ef07c222fe5c2b7df91b08fb1
Kevin <dlrghks4444@gmail.com>
2022-01-20 01:37:55 +0900

    플러터 클론 코딩

--- FlutterProject/flutter_spotify_clone/lib/main.dart
+++ FlutterProject/flutter_spotify_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spotify_clone/pages/root_app.dart';
 
 void main() {
   runApp(MaterialApp(
---
+++ FlutterProject/flutter_spotify_clone/lib/pages/root_app.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_spotify_clone/theme/colors.dart';
+
+// ignore: use_key_in_widget_constructors
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // 배경을 검정, 하단 네비게이션 추가
+      backgroundColor: black,
+      bottomNavigationBar: getFooter(),
+    );
+  }
+
+  // 위젯으로 구분하여 네비게이션 생성
+  Widget getFooter() {
+    List items = [Feather.home, Feather.book, Feather.search, Feather.settings];
+
+    return Container(
+      // 하단 네비게이션의 높이 설정
+      height: 80,
+      // 하단 네비게이션의 색상 설정
+      decoration: BoxDecoration(color: CupertinoColors.black),
+      // 하단 네비게이션의 구성 갯수와 아이콘 등의 조합 설정
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20, right: 20),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // row 줄의 리스트로 4개 구성한다. 각 구성의 아이콘 설정
+            children: List.generate(items.length, (index) {
+              return IconButton(
+                  icon: Icon(
+                    items[index],
+                    color: activeTab == index ? primary : white,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      activeTab = index;
+                    });
+                  });
+            })),
+      ),
+    );
+  }
+}
